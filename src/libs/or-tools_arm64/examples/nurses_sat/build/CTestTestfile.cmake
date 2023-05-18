baf90457fd4184dd6ce5f8f374e1b8d49562f8c1
Luquish <lucamazza02@gmail.com>
2023-05-18 18:47:00 -0300

    gitignore

--- src/libs/or-tools_arm64/examples/nurses_sat/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/nurses_sat
-# Build directory: /Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/nurses_sat/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_nurses_sat "/Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/nurses_sat/build/bin/nurses_sat")
-set_tests_properties(test_nurses_sat PROPERTIES  _BACKTRACE_TRIPLES "/Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/nurses_sat/CMakeLists.txt;81;add_test;/Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/nurses_sat/CMakeLists.txt;0;")
--- src/libs/or-tools_arm64/examples/simple_max_flow_program/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/simple_max_flow_program
-# Build directory: /Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/simple_max_flow_program/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_simple_max_flow_program "/Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/simple_max_flow_program/build/bin/simple_max_flow_program")
-set_tests_properties(test_simple_max_flow_program PROPERTIES  _BACKTRACE_TRIPLES "/Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/simple_max_flow_program/CMakeLists.txt;81;add_test;/Users/nacho/Downloads/or-tools_arm64_macOS-13.2.1_cpp_v9.6.2534/examples/simple_max_flow_program/CMakeLists.txt;0;")
